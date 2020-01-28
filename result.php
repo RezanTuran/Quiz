3730572a623de470551c653704b52553344ef9af
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-01-28 14:14:57 +0100

    Issues #3 is done

--- result.php
+++ result.php
@@ -1,6 +1,6 @@
-<?php include('./index.php'); ?>
 
 <?php
+
             $answer1 = $_POST['question-1-answers'];
             $answer2 = $_POST['question-2-answers'];
             $answer3 = $_POST['question-3-answers'];
@@ -13,19 +13,67 @@
             $answer10 = $_POST['question-10-answers'];
         
             $totalCorrect = 0;
-            
-            if ($answer1 == "C") { $totalCorrect++; }
-            if ($answer2 == "D") { $totalCorrect++; }
-            if ($answer3 == "A") { $totalCorrect++; }
-            if ($answer4 == "B") { $totalCorrect++; }
-            if ($answer5 == "D") { $totalCorrect++; }
-            if ($answer6 == "A") { $totalCorrect++; }
-            if ($answer7 == "C") { $totalCorrect++; }
-            if ($answer8 == "B") { $totalCorrect++; }
-            if ($answer9 == "C") { $totalCorrect++; }
-            if ($answer10 == "B") { $totalCorrect++; }
-            
-            echo "<div id='results'>$totalCorrect / 5 correct</div>";
-            
+
+            if ($answer1 == "C") { 
+                $totalCorrect++; 
+            }elseif($answer1 != "C"){
+                echo "Fråga 1 är fel : Rätt svar är : $answer1<br>";
+            }
+            if ($answer2 == "D") {
+                 $totalCorrect++;
+            }elseif($answer2 != "D"){
+                echo "Fråga 2 är fel : Rätt svar är : $answer2<br>";
+            }
+            if ($answer3 == "A") {
+                 $totalCorrect++;
+ 
+            }elseif($answer3 != "A"){
+                echo "Fråga 3 är fel : Rätt svar är : $answer3<br>";
+            }
+            if ($answer4 == "B") {
+                 $totalCorrect++; 
+            }elseif($answer4 != "B"){
+                echo "Fråga 4 är fel : Rätt svar är : $answer4<br>";
+            }
+            if ($answer5 == "D") {
+                 $totalCorrect++; 
+            }elseif($answer5 != "D"){
+                echo "Fråga 5 är fel : Rätt svar är : $answer5<br>";
+            }
+            if ($answer6 == "A") { 
+                $totalCorrect++;
+  
+            }elseif($answer6 != "A"){
+                echo "Fråga 6 är fel : Rätt svar är : $answer6<br>";
+            }
+            if ($answer7 == "C") {
+                 $totalCorrect++; 
+            }elseif($answer7 != "C"){
+                echo "Fråga 7 är fel : Rätt svar är : $answer7<br>";
+            }
+            if ($answer8 == "B") {
+                 $totalCorrect++;  
+            }elseif($answer8 != "B"){
+                echo "Fråga 8 är fel : Rätt svar är : $answer8<br>";
+            }
+            if ($answer9 == "C") {
+                 $totalCorrect++;
+  
+            }
+            elseif($answer9 != "C"){
+                echo "Fråga 9 är fel : Rätt svar är : $answer9<br>";
+            }
+            if ($answer10 == "B") {
+                 $totalCorrect++;  
+                }
+            elseif($answer10 != "B"){
+                echo "Fråga 10 är fel : Rätt svar är : $answer10<br>";
+            }
+
+
+            echo "<br>";
+            echo "<div id='results'><h4>$totalCorrect / 10 Rätt </h4></div>";
+
+
 ?>
 	

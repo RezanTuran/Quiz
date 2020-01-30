5e9bbfba04f14d0bc5af3dfec3ad55ea970419ff
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-01-30 10:10:38 +0100

    Issue4 is done
    Saved questions in Session

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,17 +6,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <!-- ### CDN Link For Jquery  ###-->
     <link rel="stylesheet" href="style.css">
     <script defer src="./js/main.js"></script>
     <title>Quiz</title>
 </head>
 <body>
-    
+
+<?php
+        function isChecked($questionId, $answerId) {
+            if ($_SESSION[$questionId] === $answerId ) {
+                return "checked";
+            } else {
+                return "";
+            }
+        }
+?>
 
     <form action="./result.php" method="post" id="quiz">
     
     <h1>Simple Quiz Built On PHP</h1>
-
+    
     <ol>
             
             <li>
@@ -24,22 +34,22 @@
                 <h3>WordPress är en...</h3>
                 
                 <div>
-                    <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                    <input type="radio" name="question-1-answers" <?= isChecked('question-1-answers', 'A') ?> id="question-1-answers-A" value="A" required />
                     <label for="question-1-answers-A">A) Software </label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                    <input type="radio" name="question-1-answers" <?= isChecked('question-1-answers', 'B') ?> id="question-1-answers-B" value="B" required />
                     <label for="question-1-answers-B">B) Web App</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                    <input type="radio" name="question-1-answers" <?= isChecked('question-1-answers', 'C') ?> id="question-1-answers-C" value="C" required />
                     <label for="question-1-answers-C">C) CMS</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                    <input type="radio" name="question-1-answers" <?= isChecked('question-1-answers', 'D') ?> id="question-1-answers-D" value="D" required />
                     <label for="question-1-answers-D">D) Övrigt</label>
                 </div>
             
@@ -50,22 +60,22 @@
                 <h3>Vad är SEO ?</h3>
                 
                 <div>
-                    <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                    <input type="radio" name="question-2-answers" <?= isChecked('question-2-answers', 'A') ?> id="question-2-answers-A" value="A"  required/>
                     <label for="question-2-answers-A">A) Video Editing</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                    <input type="radio" name="question-2-answers" <?= isChecked('question-2-answers', 'B') ?> id="question-2-answers-B" value="B" required />
                     <label for="question-2-answers-B">B) Graphic Designing</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                    <input type="radio" name="question-2-answers" <?= isChecked('question-2-answers', 'C') ?> id="question-2-answers-C" value="C" required />
                     <label for="question-2-answers-C">C) Web Designing</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                    <input type="radio" name="question-2-answers" <?= isChecked('question-2-answers', 'D') ?> id="question-2-answers-D" value="D"  required/>
                     <label for="question-2-answers-D">D) Digital Marketing</label>
                 </div>
             
@@ -76,22 +86,22 @@
                 <h3>PHP är en....</h3>
                 
                 <div>
-                    <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                    <input type="radio" name="question-3-answers" <?= isChecked('question-3-answers', 'A') ?> id="question-3-answers-A" value="A" required/>
                     <label for="question-3-answers-A">A) Server Sida Script</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                    <input type="radio" name="question-3-answers" <?= isChecked('question-3-answers', 'B') ?> id="question-3-answers-B" value="B" required/>
                     <label for="question-3-answers-B">B) Programmering språk</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                    <input type="radio" name="question-3-answers" <?= isChecked('question-3-answers', 'C') ?> id="question-3-answers-C" value="C" required/>
                     <label for="question-3-answers-C">C) Markup Language</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                    <input type="radio" name="question-3-answers" <?= isChecked('question-3-answers', 'D') ?> id="question-3-answers-D" value="D" required/>
                     <label for="question-3-answers-D">D) None Of Above These</label>
                 </div>
             
@@ -102,22 +112,22 @@
                 <h3>Localhost IP är..</h3>
                 
                 <div>
-                    <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                    <input type="radio" name="question-4-answers" <?= isChecked('question-4-answers', 'A') ?> id="question-4-answers-A" value="A" required/>
                     <label for="question-4-answers-A">A) 192.168.0.1</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                    <input type="radio" name="question-4-answers" <?= isChecked('question-4-answers', 'B') ?> id="question-4-answers-B" value="B" required/>
                     <label for="question-4-answers-B">B) 127.0.0.0</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                    <input type="radio" name="question-4-answers" <?= isChecked('question-4-answers', 'C') ?> id="question-4-answers-C" value="C" required/>
                     <label for="question-4-answers-C">C) 1080:80</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                    <input type="radio" name="question-4-answers" <?= isChecked('question-4-answers', 'D') ?> id="question-4-answers-D" value="D" required/>
                     <label for="question-4-answers-D">D) Any Other</label>
                 </div>
             
@@ -128,22 +138,22 @@
                 <h3>Webdevtrick är för...</h3>
                 
                 <div>
-                    <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                    <input type="radio" name="question-5-answers" <?= isChecked('question-5-answers', 'A') ?> id="question-5-answers-A" value="A" required/>
                     <label for="question-5-answers-A">A) Web Designer</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                    <input type="radio" name="question-5-answers" <?= isChecked('question-5-answers', 'B') ?> id="question-5-answers-B" value="B" required/>
                     <label for="question-5-answers-B">B) Web Developer</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                    <input type="radio" name="question-5-answers" <?= isChecked('question-5-answers', 'C') ?> id="question-5-answers-C" value="C" required/>
                     <label for="question-5-answers-C">C) Graphic Designer</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                    <input type="radio" name="question-5-answers" <?= isChecked('question-5-answers', 'D') ?> id="question-5-answers-D" value="D" required/>
                     <label for="question-5-answers-D">D) All Above These</label>
                 </div>
             
@@ -154,22 +164,22 @@
                 <h3>HTML står för...</h3>
                 
                 <div>
-                    <input type="radio" name="question-6-answers" id="question-6-answers-A" value="A" />
+                    <input type="radio" name="question-6-answers" <?= isChecked('question-6-answers', 'A') ?> id="question-6-answers-A" value="A" required/>
                     <label for="question-6-answers-A">A) HyperText Markup Language</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-6-answers" id="question-6-answers-B" value="B" />
+                    <input type="radio" name="question-6-answers" <?= isChecked('question-6-answers', 'B') ?> id="question-6-answers-B" value="B" required/>
                     <label for="question-6-answers-B">B) Hyper Markup Language</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-6-answers" id="question-6-answers-C" value="C" />
+                    <input type="radio" name="question-6-answers" <?= isChecked('question-6-answers', 'C') ?> id="question-6-answers-C" value="C" required/>
                     <label for="question-6-answers-C">C) Hypertext Transfer Protocol</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-6-answers" id="question-6-answers-D" value="D" />
+                    <input type="radio" name="question-6-answers" <?= isChecked('question-6-answers', 'D') ?> id="question-6-answers-D" value="D" required/>
                     <label for="question-6-answers-D">D) HyperTex</label>
                 </div>
             
@@ -180,22 +190,22 @@
                 <h3>HTTP står för...</h3>
                 
                 <div>
-                    <input type="radio" name="question-7-answers" id="question-7-answers-A" value="A" />
+                    <input type="radio" name="question-7-answers" <?= isChecked('question-7-answers', 'A') ?> id="question-7-answers-A" value="A" required/>
                     <label for="question-7-answers-A">A) HyperText Markup Language</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-7-answers" id="question-7-answers-B" value="B" />
+                    <input type="radio" name="question-7-answers" <?= isChecked('question-7-answers', 'B') ?>  id="question-7-answers-B" value="B" required/>
                     <label for="question-7-answers-B">B) Hyper Markup Language</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-7-answers" id="question-7-answers-C" value="C" />
+                    <input type="radio" name="question-7-answers" <?= isChecked('question-7-answers', 'C') ?>  id="question-7-answers-C" value="C" required/>
                     <label for="question-7-answers-C">C) Hypertext Transfer Protocol</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-7-answers" id="question-7-answers-D" value="D" />
+                    <input type="radio" name="question-7-answers" <?= isChecked('question-7-answers', 'D') ?>  id="question-7-answers-D" value="D" required/>
                     <label for="question-7-answers-D">D) HyperTex</label>
                 </div>
             
@@ -206,22 +216,22 @@
                 <h3>Vad är Bootstrap ?</h3>
                 
                 <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-A" value="A" />
+                    <input type="radio" name="question-8-answers" <?= isChecked('question-8-answers', 'A') ?>  id="question-8-answers-A" value="A"required />
                     <label for="question-8-answers-A">A) Javascript Biblioteket</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-B" value="B" />
+                    <input type="radio" name="question-8-answers" <?= isChecked('question-8-answers', 'B') ?> id="question-8-answers-B" value="B" required/>
                     <label for="question-8-answers-B">B) CSS Biblioteket</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-C" value="C" />
+                    <input type="radio" name="question-8-answers" <?= isChecked('question-8-answers', 'C') ?> id="question-8-answers-C" value="C" required/>
                     <label for="question-8-answers-C">C) Programmerings språk</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-D" value="D" />
+                    <input type="radio" name="question-8-answers" <?= isChecked('question-8-answers', 'D') ?> id="question-8-answers-D" value="D" required/>
                     <label for="question-8-answers-D">D) Markup Language</label>
                 </div>
             
@@ -232,22 +242,22 @@
                 <h3>Vem äger react ?</h3>
                 
                 <div>
-                    <input type="radio" name="question-9-answers" id="question-9-answers-A" value="A" />
+                    <input type="radio" name="question-9-answers" <?= isChecked('question-9-answers', 'A') ?> id="question-9-answers-A" value="A" required/>
                     <label for="question-9-answers-A">A) Microsoft</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-9-answers" id="question-9-answers-B" value="B" />
+                    <input type="radio" name="question-9-answers" <?= isChecked('question-9-answers', 'B') ?> id="question-9-answers-B" value="B" required/>
                     <label for="question-9-answers-B">B) Twitter</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-9-answers" id="question-9-answers-C" value="C" />
+                    <input type="radio" name="question-9-answers" <?= isChecked('question-9-answers', 'C') ?> id="question-9-answers-C" value="C" required/>
                     <label for="question-9-answers-C">C)Facebook</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-9-answers" id="question-9-answers-D" value="D" />
+                    <input type="radio" name="question-9-answers" <?= isChecked('question-9-answers', 'D') ?> id="question-9-answers-D" value="D" required/>
                     <label for="question-9-answers-D">D) Bill Gates</label>
                 </div>
             
@@ -258,22 +268,22 @@
                 <h3>Vilken av de är en javascript ramverk ?</h3>
                 
                 <div>
-                    <input type="radio" name="question-10-answers" id="question-10-answers-A" value="A" />
+                    <input type="radio" name="question-10-answers" <?= isChecked('question-10-answers', 'A') ?> id="question-10-answers-A" value="A" required/>
                     <label for="question-10-answers-A">A) Laravel</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-10-answers" id="question-10-answers-B" value="B" />
+                    <input type="radio" name="question-10-answers" <?= isChecked('question-10-answers', 'B') ?> id="question-10-answers-B" value="B" required/>
                     <label for="question-10-answers-B">B) Vue</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-10-answers" id="question-10-answers-C" value="C" />
+                    <input type="radio" name="question-10-answers" <?= isChecked('question-10-answers', 'C') ?> id="question-10-answers-C" value="C" required/>
                     <label for="question-10-answers-C">C) Flask</label>
                 </div>
                 
                 <div>
-                    <input type="radio" name="question-10-answers" id="question-10-answers-D" value="D" />
+                    <input type="radio" name="question-10-answers" <?= isChecked('question-10-answers', 'D') ?> id="question-10-answers-D" value="D" required/>
                     <label for="question-10-answers-D">D) .Net</label>
                 </div>
             
--- result.php
+++ result.php
@@ -1,6 +1,8 @@
 
 <?php
 
+session_start();
+
             $answer1 = $_POST['question-1-answers'];
             $answer2 = $_POST['question-2-answers'];
             $answer3 = $_POST['question-3-answers'];
@@ -11,69 +13,88 @@
             $answer8 = $_POST['question-8-answers'];
             $answer9 = $_POST['question-9-answers'];
             $answer10 = $_POST['question-10-answers'];
-        
+
+            $_SESSION['question-1-answers'] = $answer1;
+            $_SESSION['question-2-answers'] = $answer2;
+            $_SESSION['question-3-answers'] = $answer3;
+            $_SESSION['question-4-answers'] = $answer4;
+            $_SESSION['question-5-answers'] = $answer5;
+            $_SESSION['question-6-answers'] = $answer6;
+            $_SESSION['question-7-answers'] = $answer7;
+            $_SESSION['question-8-answers'] = $answer8;
+            $_SESSION['question-9-answers'] = $answer9;
+            $_SESSION['question-10-answers'] = $answer10;
+
+
             $totalCorrect = 0;
 
             if ($answer1 == "C") { 
-                $totalCorrect++; 
+                $totalCorrect++;
+                echo "Fråga 1 är rätt : $answer1<br>" ;
             }elseif($answer1 != "C"){
-                echo "Fråga 1 är fel : Rätt svar är : $answer1<br>";
+                echo "Fråga 1 är fel</br>";
             }
             if ($answer2 == "D") {
                  $totalCorrect++;
+                 echo "Fråga 2 är rätt : $answer2<br>";
             }elseif($answer2 != "D"){
-                echo "Fråga 2 är fel : Rätt svar är : $answer2<br>";
+                echo "Fråga 2 är fel</br>";
             }
             if ($answer3 == "A") {
                  $totalCorrect++;
- 
+                echo "Fråga 3 är rätt : $answer3<br>";
             }elseif($answer3 != "A"){
-                echo "Fråga 3 är fel : Rätt svar är : $answer3<br>";
+                echo "Fråga 3 är fel</br>";
             }
-            if ($answer4 == "B") {
+            if ($answer4 == "D") {
                  $totalCorrect++; 
-            }elseif($answer4 != "B"){
-                echo "Fråga 4 är fel : Rätt svar är : $answer4<br>";
+                 echo "Fråga 4 är rätt : $answer4<br>";
+            }elseif($answer4 != "D"){
+                echo "Fråga 4 är fel</br>";
             }
             if ($answer5 == "D") {
                  $totalCorrect++; 
+                 echo "Fråga 5 är rätt : $answer5<br>";
             }elseif($answer5 != "D"){
-                echo "Fråga 5 är fel : Rätt svar är : $answer5<br>";
+                echo "Fråga 5 är fel</br>";
             }
             if ($answer6 == "A") { 
                 $totalCorrect++;
-  
+                echo "Fråga 6 är rätt : $answer6<br>";
             }elseif($answer6 != "A"){
-                echo "Fråga 6 är fel : Rätt svar är : $answer6<br>";
+                echo "Fråga 6 är fel</br>";
             }
             if ($answer7 == "C") {
                  $totalCorrect++; 
+                 echo "Fråga 7 är rätt : $answer7<br>";
             }elseif($answer7 != "C"){
-                echo "Fråga 7 är fel : Rätt svar är : $answer7<br>";
+                echo "Fråga 7 är fel</br>";
             }
             if ($answer8 == "B") {
                  $totalCorrect++;  
+                 echo "Fråga 8 är rätt : $answer8<br>";
             }elseif($answer8 != "B"){
-                echo "Fråga 8 är fel : Rätt svar är : $answer8<br>";
+                echo "Fråga 8 är fel</br>";
             }
             if ($answer9 == "C") {
                  $totalCorrect++;
-  
+                echo "Fråga 9 är rätt : $answer9<br>";
             }
             elseif($answer9 != "C"){
-                echo "Fråga 9 är fel : Rätt svar är : $answer9<br>";
+                echo "Fråga 9 är fel</br>";
             }
             if ($answer10 == "B") {
                  $totalCorrect++;  
+                 echo "Fråga 10 är rätt : $answer10<br>";
                 }
             elseif($answer10 != "B"){
-                echo "Fråga 10 är fel : Rätt svar är : $answer10<br>";
+                echo "Fråga 10 är fel</br>";
             }
 
 
             echo "<br>";
             echo "<div id='results'><h4>$totalCorrect / 10 Rätt </h4></div>";
-
-
 ?>
+
+<a href="index.php"><button>Tillbaka</button></a>
 	
